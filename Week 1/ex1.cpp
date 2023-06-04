aea21da026d975c0dc2d00f44bab3d0d636dd93a
Mewww2k4 <121652389+Mewww2k4@users.noreply.github.com>
2023-06-05 00:37:59 +0700

    update file

--- Week 1/ex1.cpp
+++ Week 1/ex1.cpp
@@ -31,14 +31,62 @@ int Max(int a[], int n)    // Find element max
 	}
 	return max ;
 }
+int Swap(int &a, int &b)
+{
+	int temp = a;
+	a= b;
+    b = temp;
+}
+void bubbleArrays(int a[], int n)
+{
+	for(int i = 0 ; i < n - 1 ; i++){
+		for(int j = 0 ; j < n - i - 1; j++)
+		{
+			if(a[j] > a[j+1])
+			Swap(a[j],a[j+1]);
+		}
+	}
+}
+void Output(int a[], int n)
+{   cout<<"Sorted Arrays : ";
+	for(int i = 0 ; i < n ; i++)
+	{
+		cout << a[i] <<"   " ;
+	}
+}
+ struct Rectangle{
+ 	double height;
+ 	double width;
+ };
+ void input(Rectangle &r)
+ {
+ 	cout <<"\nInput the height and the width : ";
+ 	cin >> r.height >> r.width;
+ }
+ void display(Rectangle &r)
+ {
+ 	cout <<"The height : "<< r.height<< endl;
+ 	cout <<"The width : " << r.width << endl;
+ }
+ double displayArea(Rectangle &r)
+ {
+ 	return (r.height * r.width);
+ }
+ void edit(Rectangle &r){
+ 	cout <<"Input the height new : " ;
+ 	cin >> r.height;
+ 	cout <<"Input the width new : ";
+ 	cin >> r.width;
+ }
 int main(){
 	//1.Integer Arrays
 	 int a[100];
 	 int n, search;
+	 cout <<"Input element n : "   ;
 	 cin >> n ;//Input quantity elment    (length)
 	 Input(a,n) ;
-	 
-	 
+
+
 	 cout<<"Input number to find : " ; cin >> search;
 	 int result = search_integer(a,n,search);
 	 if(result == 1)
@@ -48,10 +96,21 @@ int main(){
 	 else {
 	 	cout <<"Integers not found in the list !!!"<<endl;
 	 }
-	
+
 	//2.Max
-	int result1 = Max(a,n);
-	cout <<"The Integer Max :  "<< result1 <<endl;
-	
+	cout <<"The Integer Max :  "<< Max(a,n) <<endl;
+
+	//3.Bubble Sort
+	bubbleArrays(a,n);
+	Output(a,n);
+
+	//4. Struct Rectangle
+	Rectangle r;
+	 input(r);
+	 display(r);
+	 cout <<"The Area of Rectangle is : " << displayArea(r) << endl;
+	 edit(r);
+	 display(r);
+	 cout <<"The Area of Rectangle new is : " << displayArea(r)<<endl;
 	return 0;
 }
